9dc8d2528e96b6c7a4db22f1c31cb6f3cb5a8d2a
cxp1539 <457714145@qq.com>
2019-01-17 10:11:12 +0800

    add code

---
+++ code/Class3.php
@@ -0,0 +1,107 @@
+<?php
+
+    class Ioc
+    {
+        public $binding = [];
+        public function bind($abstract, $concrete)
+        {
+            if (!$concrete instanceof Closure) {
+                $concrete = function ($ioc) use ($concrete) {
+                    return $ioc->build($concrete);
+                };
+            }
+            $this->binding[$abstract]['concrete'] = $concrete;
+        }
+        public function make($abstract)
+        {
+            $concrete = $this->binding[$abstract]['concrete'];
+            return $concrete($this);
+        }
+        public function build($concrete) {
+            $reflector = new ReflectionClass($concrete);
+            $constructor = $reflector->getConstructor();
+            if(is_null($constructor)) {
+                return $reflector->newInstance();
+            }else {
+                $dependencies = $constructor->getParameters();
+                $instances = $this->getDependencies($dependencies);
+                return $reflector->newInstanceArgs($instances);
+            }
+        }
+        protected function getDependencies($paramters) {
+            $dependencies = [];
+            foreach ($paramters as $paramter) {
+                $dependencies[] = $this->make($paramter->getClass()->name);
+            }
+            return $dependencies;
+        }
+    }
+    interface log
+    {
+        public function write();
+    }
+    // 文件记录日志
+    class FileLog implements Log
+    {
+        public function write(){
+            echo 'file log write...';
+        }
+    }
+    // 数据库记录日志
+    class DatabaseLog implements Log
+    {
+        public function write(){
+            echo 'database log write...';
+        }
+    }
+    class User
+    {
+        protected $log;
+        public function __construct(Log $log)
+        {
+            $this->log = $log;
+        }
+        public function login()
+        {
+            // 登录成功，记录登录日志
+            echo 'login success...';
+            $this->log->write();
+        }
+    }
+
+    class UserFacade
+    {
+
+        protected static $ioc;
+
+        public static function setFacadeIoc($ioc)
+        {
+            static::$ioc = $ioc;
+        }
+
+
+        protected static function getFacadeAccessor()
+        {
+            return 'user';
+        }
+
+
+        public static function __callStatic($method, $args)
+        {
+            $instance = static::$ioc->make(static::getFacadeAccessor());
+            return $instance->$method(...$args);
+        }
+
+    }
+
+    //实例化IoC容器
+
+    $ioc = new Ioc();
+    $ioc->bind('log','FileLog');
+    $ioc->bind('user','User');
+
+    UserFacade::setFacadeIoc($ioc);
+
+    UserFacade::login();
+
+    exit;
