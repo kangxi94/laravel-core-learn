6a4c95ff739fe1a3bdb89cbb10320a4fa9eea828
cxp1539 <457714145@qq.com>
2019-01-17 11:16:14 +0800

    add code

---
+++ code/Class5.php
@@ -0,0 +1,50 @@
+<?php
+
+    interface Milldeware {
+        public static function handle(Closure $next);
+    }
+
+    class VerfiyCsrfToekn implements Milldeware {
+
+        public static function handle(Closure $next)
+        {
+            echo '验证csrf Token <br>';
+            $next();
+        }
+    }
+
+    class VerfiyAuth implements Milldeware {
+
+        public static function handle(Closure $next)
+        {
+            echo '验证是否登录 <br>';
+            $next();
+        }
+    }
+
+    class SetCookie implements Milldeware {
+        public static function handle(Closure $next)
+        {
+            $next();
+            echo '设置cookie信息！';
+        }
+    }
+
+    $handle = function() {
+        echo '当前要执行的程序!';
+    };
+
+
+    $pipe_arr = [
+        'VerfiyCsrfToekn',
+        'VerfiyAuth',
+        'SetCookie'
+    ];
+
+    $callback = array_reduce($pipe_arr,function($stack,$pipe) {
+        return function() use($stack,$pipe){
+            return $pipe::handle($stack);
+        };
+    },$handle);
+
+    call_user_func($callback);
